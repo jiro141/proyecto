555259eeb4e22ca5768d1898cc79d269a81341a0
jiro141 <felipe.colmenaresvzla@gmail.com>
2026-05-13 09:44:27 -0400

    chore: update backend URL to hermabe.cloud

--- FrontEnd/src/api/AuthApi.jsx
+++ FrontEnd/src/api/AuthApi.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 // Base URL de tu backend Django
-const BASE_URL = "http://2.24.94.171:8000/api";
+const BASE_URL = "https://hermabe.cloud/api";
 
 // Mensajes de error en español
 const getErrorMessage = (error) => {
